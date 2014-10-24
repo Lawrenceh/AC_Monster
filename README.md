704971bc9bcfd4bdb43443c7657dd538f796b478
AC_Monster <lawrencehuangyiling@gmail.com>
2014-10-24 15:52:39 -0400

    Oct 24

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 AC_Monster
 ==========
 Alogrithms & Data Structures & Design Patterns.
+- mergesort
+- heapsort
+- kmp & bm
--- n_factorial_ending_zeros/n_factorial_ending_zeros.cpp
+++ n_factorial_ending_zeros/n_factorial_ending_zeros.cpp
@@ -1,7 +1,7 @@
 int CalZeroNum(int n) {
     int num = 0;
     int b = 1;
-    while(1) {
+    while (1) {
         b *= 5;
         num += n / b;
         if (b > n) break;
